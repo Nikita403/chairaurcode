5824d115e806f839287bc65bea5420c0c342646a
Nikita403 <57703299+Nikita403@users.noreply.github.com>
2025-03-07 07:35:38 +0000

    3files

---
+++ datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newr version
+//alert(3 + 3); we are using nodejs not browser 
+// code should be write in a way look like readable
+
+let name = "Nikki";
+let age = 30;
+let isloggedIn = false;
+let state = null;
+console.log(typeof undefined); // type of undefined is undefind
+console.log(typeof null); //typeof null is not null but its object in js
+
+
+
+
+// number => 2 to power 53 or 52
+// bigInt = for larger number
+// string = ""
+// boolean = true/false
+// null => stand alone value
+// undefined => eclared but not assigned any value
+// symbol => unique
+
+//object
+//array 
---
+++ variable.js
@@ -0,0 +1,19 @@
+const accountId = 12345;
+let email = "abc@google.com";
+var accountPassword = "123";
+acountCity = "Jaipur"
+let accountState;
+//noaccountId = 43211;
+email = "example@gmail.com";
+accountPassword = "567";
+acountCity = "Raipur"
+console.log(accountId);
+console.log(email);
+console.log(accountPassword);
+console.log(acountCity);
+console.table([accountId,email,accountPassword,acountCity,accountState]);
+
+/*
+Please don't prefer var because of issue of block scope and functional scope
+*/
+
